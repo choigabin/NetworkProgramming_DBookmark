0191dee856010f7b1439f676aa019bfef80d7291
choigabin <s2019217@e-mirim.hs.kr>
2021-05-06 09:54:42 +0900

    주석 추가

--- bookmark/models.py
+++ bookmark/models.py
@@ -9,5 +9,5 @@ class Bookmark(models.Model):
     def __str__(self):
         return f'{self.name} | {self.url}'
 
-    def get_absolute_url(self):
+    def get_absolute_url(self): #views.py UpdateView의 success_url = reverse_lazy('bookmark:list')를 주석처리해주면, 수정을 완료하고 detail 화면으로 넘거암
         return resolve_url('bookmark:detail', pk=self.pk)
